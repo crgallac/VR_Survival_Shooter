d2c18f87f5319ce612a57f19b7509c56c56281a0
unknown <cr_gallac@hotmail.com>
2018-10-26 20:46:44 -0400

    first commit of working build for Halloweekend party

---
+++ VR_SurvivalShooter/Assets/_Complete-Game/Scripts/Managers/HighScoreManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace CompleteProject
+{
+    public class HighScoreManager : MonoBehaviour
+    {
+
+        Text text;                      // Reference to the Text component.
+
+        public static int high_score;        // The player's score.
+        public static bool high_score_flag; 
+
+
+
+
+        void Awake()
+        {
+            // Set up the reference.
+            text = GetComponent<Text>();
+
+            // Reset the score.
+            high_score = 0;
+        }
+
+
+        void Update()
+        {
+            // Set the displayed text to be the word "Score" followed by the score value.
+            text.text = "High Score: " + high_score;
+        }
+    }
+}
